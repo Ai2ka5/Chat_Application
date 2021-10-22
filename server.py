402e1489d6f565e4414b347d6140ac6f44e523b8
Ai2ka5 <aidukas.mantas@gmail.com>
2021-10-22 12:02:24 +0300

    Created function to handle client

--- server.py
+++ server.py
@@ -4,6 +4,22 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 LISTENER_LIMIT = 5
+active_clients = []
+
+
+def send_message_to_all(from_username, message):
+    pass
+
+
+def client_handler(client):
+    while 1:
+        username = client.recv(2048).decode('utf-8')
+        if username != "":
+            active_clients.append((username, client))
+
+        else:
+            print("Client username is empty")
+
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,9 +33,11 @@ def main():
     server.listen(LISTENER_LIMIT)
 
     while 1:
-
         client, address = server.accept()
         print(f"Successfully connected to client {address[0]} {address[1]}")
+
+        threading.Thread(target=client_handler, args=(client,)).start()
+
 
 if __name__ == "__main__":
     main()
